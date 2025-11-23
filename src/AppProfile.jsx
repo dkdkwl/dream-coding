f8667c5ca7f9968b2c403fc9bf42c5c4aab50b07
dkdkwl <dkdkwl@naver.com>
2025-11-23 21:48:43 +0900

    props활용하는 방법

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -2,12 +2,23 @@ import './App.css'
 import Profile from './components/Profile'
 
 function AppProfile() {
-
   return (
     <>
-      <Profile></Profile>
-      <Profile></Profile>
-      <Profile></Profile>
+      <Profile 
+      image="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+      name="Jane Doe"
+      title="Software Engineer"
+      />
+      <Profile
+        image="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+        name="John Smith"
+        title="Product Manager"
+      />
+      <Profile
+        image="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+        name="Emily Johnson"
+        title="UX Designer"        
+      />
     </>
   )
 }
