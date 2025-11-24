85d0d6610c2db1b9feae1f8545984ebb118a1cd4
dkdkwl <dkdkwl@naver.com>
2025-11-24 12:49:22 +0900

    새로 들어온 개발자만 new 붙여주기

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
-export default function Profile({image,name,title}) {
+export default function Profile({image,name,title,status}) {
   return (
     <div className='profile'>
-      <img className='photo'
-       src={image}
-       alt="" />
-      <h1>{name}</h1>
-      <p>{title}</p>
+      <div className="profile-inner">
+        {status && <div className="badge">new</div>}
+        <img className='photo'
+         src={image}
+         alt="" />
+        <h1>{name}</h1>
+        <p>{title}</p>
+      </div>
     </div>
   )
 }
