ec0c559cb397153a8e7596bbf6f56d4cf1bc4145
dkdkwl <dkdkwl@naver.com>
2025-11-25 22:33:45 +0900

    클릭 했을때 수치 올리기

---
+++ src/components/Counter.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+function handleClick() {
+    setCount(count + 1);
+}
+  return (
+    <div className='counter'>
+        <span className='number'>{count}</span>
+        <button className='button' onClick={handleClick}>add +</button>
+    </div>
+  )
+
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppProfile from './AppProfile.jsx'
+import AppCounter from './AppCounter.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppProfile />
+    <AppCounter />
   </StrictMode>,
 )
