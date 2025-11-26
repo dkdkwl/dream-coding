bb1d728984ac374ce9581a6a027feeff436e9ada
dkdkwl <dkdkwl@naver.com>
2025-11-26 13:01:32 +0900

    토탈카운트, 클릭한 요소 숫자 올라가게 수정

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 
-export default function Counter() {
+export default function Counter({total, onClick}) {
     const [count, setCount] = useState(0);
-function handleClick() {
-    setCount(count + 1);
-}
+    function handleClick() {
+        setCount(prev=>prev + 1);
+        onClick();
+    }
   return (
     <div className='counter'>
-        <span className='number'>{count}</span>
+        <span className='number'>{count} / {total}</span>
         <button className='button' onClick={handleClick}>add +</button>
     </div>
   )
