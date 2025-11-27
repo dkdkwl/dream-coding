658cb3e6e0eeb1301492c27a97dbc35bbeb9e503
dkdkwl <dkdkwl@naver.com>
2025-11-27 13:09:40 +0900

    스타일 넣고서 주는방법으로 고민중

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -4,10 +4,15 @@ import './App.css';
 export default function AppXY() {
     const [mouseX, setMouseX] = useState(0);
     const [mouseY, setMouseY] = useState(0);
-    
+    function handleMouseMove(event) {
+        setMouseX(event.clientX);
+        setMouseY(event.clientY);
+        console.log(mouseX,mouseY)
+    }
+
   return (
-    <div onMouseMove={console.log("123")} className='container'>
-      <div className='pointer' />
+    <div onMouseMove={handleMouseMove} className='container'>
+      <div style={{position:'30px'}} className='pointer' />
     </div>
   );
 }
