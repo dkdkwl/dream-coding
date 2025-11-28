15c88a72ae6f7284f277000b0f3d99518842b0d2
dkdkwl <dkdkwl@naver.com>
2025-11-28 12:41:59 +0900

    마우스 따라다니게 만들기

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
-import './App.css';
-
+import './AppXY.css';
 export default function AppXY() {
-    const [mouseX, setMouseX] = useState(0);
-    const [mouseY, setMouseY] = useState(0);
-    function handleMouseMove(event) {
-        setMouseX(event.clientX);
-        setMouseY(event.clientY);
-        console.log(mouseX,mouseY)
-    }
+  const [mouseX, setMouseX] = useState(0);
+  const [mouseY, setMouseY] = useState(0);
+  function handleMouseMove(event) {
+      setMouseX(event.clientX);
+      setMouseY(event.clientY);
+      console.log(mouseX,mouseY)
+  }
 
-  return (
-    <div onMouseMove={handleMouseMove} className='container'>
-      <div style={{position:'30px'}} className='pointer' />
-    </div>
-  );
+return (
+  <div onMouseMove={handleMouseMove} className='container'>
+    <div style={{ transform: `translate(${mouseX}px, ${mouseY}px)` }} className='pointer' />
+  </div>
+);
 }
