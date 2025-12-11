ec55212798b99afad4b2c8870ed89179b82daa04
dkdkwl <dkdkwl@naver.com>
2025-12-11 12:59:45 +0900

    멘도 이름 바꾸고 타이틀 이름도 바꿔보기

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppXY from './AppXY.jsx'
+import AppMentor from './AppMentor.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppXY />
+    <AppMentor />
   </StrictMode>,
 )
