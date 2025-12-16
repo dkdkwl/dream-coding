0f82087dbcb6b62b2d50a9fe91b146868d97a768
dkdkwl <dkdkwl@naver.com>
2025-12-16 21:53:41 +0900

    usereducer 작업 해보기

--- src/reducer/person-reducer.js
+++ src/reducer/person-reducer.js
@@ -1,7 +1,31 @@
-function personReducer(person, action){
+export default  function personReducer(person, action){
     switch(action.type){
         case 'updated': {
             const {prev, current} = action;
+            return{
+                ...person,
+                    mentors: person.mentors.map((mentor) => {
+                        if (mentor.name === prev) {
+                        return { ...mentor, name: current };
+                        }
+                        return mentor;
+                }),
+            };
+        }
+        case 'added': {
+            return{
+                ...person,
+                mentors: [{ name: action.mentor.name, title: action.mentor.title }, ...person.mentors],
+                };
+            }
+        case 'deleted': {
+            return {
+                ...person,
+                mentors: person.mentors.filter((m) => m.name !== action.name),
+            };
+        }
+        default: {
+            throw Error(`알 수 없는 액션 타입입니다. ${action.type}`);
         }
     }
 }
