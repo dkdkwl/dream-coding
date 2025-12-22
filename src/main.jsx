565ba2495281930d12a5c5aaa042dc2202475ae4
dkdkwl <dkdkwl@naver.com>
2025-12-22 12:32:37 +0900

    context만들기 작업

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppMentor from './AppMentor.jsx'
+import AppTheme from './AppTheme'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppMentor />
+        <AppTheme />
   </StrictMode>,
 )
+~
