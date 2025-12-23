55e7ea0f29bac3856f5b8a522fc1f1dff850b8a8
dkdkwl <dkdkwl@naver.com>
2025-12-23 12:51:17 +0900

    styled component 사용법 확인하기

--- src/AppProducts.jsx
+++ src/AppProducts.jsx
@@ -2,6 +2,26 @@ import React, { useState } from 'react';
 import './App.css';
 import Products from './components/Products';
 import styles from './assets/css/button2.module.css';
+import styled,{css} from 'styled-components';
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`;
+const Button = styled.button`
+background:transparent;
+border-radius:3px;
+border:2px solid black;
+color:black;
+margin:0.5em 1em;
+padding:0.25em 1em;
+${(props)=>
+    props.primary && css`
+    background:black;
+    color:white;
+    `}
+`
 
 export default function AppProducts() {
   const [showProducts, setShowProducts] = useState(true);
@@ -10,6 +30,9 @@ export default function AppProducts() {
     <div>
       {showProducts && <Products />}
       <button className={styles.button} onClick={() => setShowProducts((show) => !show)}>Toggle</button>
+      <Container>
+        <Button primary>Toggle</Button>
+      </Container>
     </div>
   );
 }
